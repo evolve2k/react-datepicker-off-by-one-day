3c97aad39ef106958c7e40eb51ac0f76bfbb304b
Richie Khoo <evolve2k@gmail.com>
2020-12-22 10:54:41 +1030

    Added react-datepicker example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
 import './App.css';
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
+  const [startDate, setStartDate] = useState('');
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>react-datepicker Off By One Day Bug</h1>
+        <a href="https://github.com/Hacker0x01/react-datepicker/issues/1018">
+          <h2>Date Selected is One Day Off #1018</h2>
+        </a>
+        <p>Enter Date in Select Box Below</p>
+        <DatePicker 
+          type="datetime"
+          selected={startDate} 
+          onChange={date => setStartDate(date)} 
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,5 +33,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
